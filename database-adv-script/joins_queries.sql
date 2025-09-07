31401601c2a64dc20c41e2c5a85de59c5b1e4366
Skai <tyronekisienya01@gmail.com>
2025-09-07 16:31:38 +0300

    a bit of cleaning

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,6 @@
 -- write a query using Inner Join to retrieve all bookings and the respective
 -- users who made those bookings
 
-select b.booking_id, b.start_date, b.end_date, b.status, u.first_name, u.last_name, u.email, u.phone_number from Booking INNER JOIN User  u ON b.user_id = u.user_id
+select b.booking_id, b.start_date, b.end_date, b.status, u.first_name, u.last_name, u.email, u.phone_number
+from Booking
+INNER JOIN User  u ON b.user_id = u.user_id
